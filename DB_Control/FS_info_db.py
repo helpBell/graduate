9b7af3425b9017629815444dd6f04a5430ddfd63
hhelpbell <hhelpbell@gmail.com>
2020-09-23 22:14:30 +0900

    DB files over Max Size of Github

---
+++ DB_Control/FS_info_db.py
@@ -0,0 +1,181 @@
+import pandas as pd
+import sqlite3
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, TimeoutException
+import time
+import datetime
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless') # headless: 브라우저X
+driver = webdriver.Chrome('C:\\Users\hawoo\Desktop\SKKU CS\Graduate\chromedriver.exe', options=options)
+
+def crawl_fs_data(Symbol):
+    fs_url=f"http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode={Symbol}"
+    driver.get(fs_url)
+
+    fs_page = driver.page_source
+    fs_tables = pd.read_html(fs_page)
+
+    for i in range(len(fs_tables)):
+        for j in range(20):
+            try: driver.find_element_by_xpath(f'//*[@id="grid{i+1}_{j+1}"]').click()
+            except NoSuchElementException: continue
+
+    fs_page = driver.page_source
+    fs_tables = pd.read_html(fs_page)
+
+    # 포괄손익계산서 Income statement IS
+    fs_is = fs_tables[0]
+    # 인덱스 이름(계정목록들)을 지정해주고 빼주기. 첫째 열이 IFRS(연결), IFRS(개별), GAAP(개별)과 같이 상황에 따라 다름
+    first_column=fs_is.columns[0]
+    fs_is.index=fs_is[first_column].values
+    # inplace는 변수 대입 불필요. axis=1은 열기준
+    fs_is.drop([first_column,'전년동기','전년동기(%)'], inplace=True, axis=1)
+    # 행 목록 필요없는 부분 제거
+    for i, name in enumerate(fs_is.index):
+        if '참여한' in name:
+            name = '*' + name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_is.rename(index={str(fs_is.index[i]):str(name)}, inplace=True)
+
+    # 재무상태표 Balance Sheet BS
+    fs_bs = fs_tables[2]
+    # 인덱스 이름(계정목록들)을 지정해주고 빼주기. 첫째 열이 IFRS(연결), IFRS(개별), GAAP(개별)과 같이 상황에 따라 다름
+    first_column=fs_bs.columns[0]
+    fs_bs.index=fs_bs[first_column].values
+    # inplace는 변수 대입 불필요. axis=1은 열기준
+    fs_bs.drop([first_column], inplace=True, axis=1)
+    # 행 목록 필요없는 부분 제거
+    for i, name in enumerate(fs_bs.index):
+        if '참여한' in name:
+            name = '*'+name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_bs.rename(index={str(fs_bs.index[i]):str(name)}, inplace=True)
+
+    # 현금흐름표 statement of cash flow CF
+    fs_cf = fs_tables[4]
+    # 인덱스 이름(계정목록들)을 지정해주고 빼주기. 첫째 열이 IFRS(연결), IFRS(개별), GAAP(개별)과 같이 상황에 따라 다름
+    first_column=fs_cf.columns[0]
+    fs_cf.index=fs_cf[first_column].values
+    # inplace는 변수 대입 불필요. axis=1은 열기준
+    fs_cf.drop([first_column], inplace=True, axis=1)
+    # 행 목록 필요없는 부분 제거
+    for i, name in enumerate(fs_cf.index):
+        if '참여한' in name:
+            name = '*'+name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_cf.rename(index={str(fs_cf.index[i]):str(name)}, inplace=True)
+
+    fs_df=pd.concat([fs_is,fs_bs,fs_cf])
+    return fs_df
+
+# 증권 종목쪽 데이터는 재무내용이 달라서 ElementNotInteractableException 에러발생 따로 처리
+def crawl_er_fs_data(Symbol):
+
+    fs_url=f"http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode={Symbol}"
+    driver.get(fs_url)
+
+    fs_page = driver.page_source
+    fs_tables = pd.read_html(fs_page)
+
+    for i in range(len(fs_tables)):
+        for j in range(45):
+            # print(i, j)
+            try :
+                driver.find_element_by_xpath(f'//*[@id="grid{i+1}_{j+1}"]').click()
+
+            except NoSuchElementException :
+                continue
+            except ElementNotInteractableException:
+                try:
+                    print('wait...click')
+                    element = WebDriverWait(driver, 2).until(
+                EC.element_to_be_clickable((By.XPATH, f'//*[@id="grid{i+1}_{j+1}"]')))
+                    element.click()
+                    print('click ok')
+                except TimeoutException:
+                    print("timeout")
+                    continue
+
+    fs_page = driver.page_source
+    fs_tables = pd.read_html(fs_page)
+
+    fs_is = fs_tables[0]
+    first_column=fs_is.columns[0]
+    fs_is.index=fs_is[first_column].values
+    fs_is.drop([first_column,'전년동기','전년동기(%)'], inplace=True, axis=1)
+    for i, name in enumerate(fs_is.index):
+        if '참여한' in name:
+            name = '*'+name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_is.rename(index={str(fs_is.index[i]):str(name)}, inplace=True)
+
+    fs_bs = fs_tables[2]
+    first_column=fs_bs.columns[0]
+    fs_bs.index=fs_bs[first_column].values
+    fs_bs.drop([first_column], inplace=True, axis=1)
+    for i, name in enumerate(fs_bs.index):
+        if '참여한' in name:
+            name = '*'+name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_bs.rename(index={str(fs_bs.index[i]):str(name)}, inplace=True)
+
+    fs_cf = fs_tables[4]
+    first_column=fs_cf.columns[0]
+    fs_cf.index=fs_cf[first_column].values
+    fs_cf.drop([first_column], inplace=True, axis=1)
+    for i, name in enumerate(fs_cf.index):
+        if '참여한' in name:
+            name = '*'+name.strip().replace('계산에 참여한 계정 감추기','')
+            fs_cf.rename(index={str(fs_cf.index[i]):str(name)}, inplace=True)
+
+    fs_df=pd.concat([fs_is,fs_bs,fs_cf])
+    return fs_df
+
+def func_FSinfo(name):
+    # SQLite3 DB 불러오기
+    con = sqlite3.connect("전체종목정보.db")
+    code_data = pd.read_sql("SELECT * FROM CorpList", con)
+    con.close()
+
+    code_data = code_data[['Symbol', 'Name']]
+
+    # 재무제표 SQLite3 DB로 저장하기
+    con = sqlite3.connect(name)
+
+    for i, code in enumerate(code_data['Symbol']):
+        try:
+            fs_data = crawl_fs_data('A'+str(code))
+            fs_data.to_sql(code, con, if_exists="replace", index=True)
+        except ValueError:
+            print("temporary error")
+            print(code + " Value error")
+            continue
+        # 투자회사 종목 096300에 에러. fnguide상으로는 안내페이지 나옴
+        except ImportError:
+            print("temporary error")
+            print(code + " Import error")
+            continue
+        # 이상한 종목 ex. 094800 맵스리얼티1 168490 한국패러랠 같은 종목은 fnguide에 정보가 없어서(안내페이지 x) 함수에서 오류 (전년동기, %)
+        except KeyError:
+            print("temporary error")
+            print(code + " key error")
+            continue
+        # 위 keyerror랑 같은 문제, 함수 구성에 따라 type error 발생 (참여한 부분)
+        except TypeError:
+            print("temporary error")
+            print(code + " Type error")
+            continue
+        # 증권 종목 별도 함수이용 처리
+        except ElementNotInteractableException:
+            print(code + "not interactable")
+            fs_data = crawl_er_fs_data('A'+str(code))
+            fs_data.to_sql(code, con, if_exists="replace", index=True)
+            continue
+    con.close()
+    print(name + " is updated")
+
+if __name__=="__main__":
+    func_FSinfo("재무제표정보.db")
+
+
--- LSTM_Stock.py
+++ LSTM_Stock.py
@@ -12,16 +12,16 @@ from sklearn.preprocessing import MinMaxScaler
 def LSTM_Stock(code):
     # 1000일치 데이터 학습
     Num = 1000
-    # SQLite3를 이용해 해당 코드 종목 가격 데이터를 불러옵니다.
+    # SQLite3를 이용해 해당 코드 종목 가격 데이터 로드
     con = sqlite3.connect("종목별_가격정보.db")
     data = pd.read_sql(f"SELECT * FROM '{code}' order by date LIMIT {Num}", con)
     con.close()
 
-    # 당일 고가와 저가 사이의 중간 값을 추청합니다.
+    # 당일 고가와 저가 사이의 중간 값을 추정
     midPrice = (data['High'].values + data['Low'].values) / 2
 
 
-    # 학습할 윈도우 사이즈를 지정합니다.
+    # 학습할 윈도우 사이즈 지정
     winSize = 30
     winLen = winSize + 1
     
@@ -29,7 +29,7 @@ def LSTM_Stock(code):
     for i in range(len(midPrice) - winLen):
         result.append(midPrice[i: i+winLen])
 
-    # 윈도우를 정규화 합니다.
+    # 윈도우 데이터 정규화
     normlData = []
     for window in result:
         normlWindow = [((float(w) / float(window[0])) - 1) for w in window]
@@ -37,7 +37,7 @@ def LSTM_Stock(code):
 
     result = np.array(normlData)
 
-    # Train Set과 Test Set으로 분리합니다.
+    # Train Set과 Test Set으로 분리
     row = int(round(result.shape[0] * 0.9))
     train = result[:row, :]
     np.random.shuffle(train)
@@ -50,10 +50,13 @@ def LSTM_Stock(code):
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
     y_test = result[row:, -1]
 
-    # LSTM 모델을 설계합니다.
+    # LSTM 모델 설계
     model = Sequential()
     model.add(LSTM(30, return_sequences=True, input_shape=(30, 1)))
-    model.add(LSTM(64, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(LSTM(100, return_sequences=False))
+    model.add(Dropout(0.2))
+    # model.add(LSTM(64, return_sequences=False))
     model.add(Dense(1, activation='linear'))
     model.compile(loss='mse', optimizer='rmsprop')
     model.summary()
@@ -62,6 +65,9 @@ def LSTM_Stock(code):
     model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=10, epochs=20)
 
     prdcData = model.predict(x_test)
+
+    # prdcData = lstm.predict_sequences_multiple(model, x_test, 30, 30)
+    # lstm.plot_results_multiple(prdcData, y_test, 30)
 
     # 결과 데이터를 나타내기 위한 코드
     fig = plt.figure(facecolor='white', figsize=(20, 10))
